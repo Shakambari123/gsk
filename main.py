a2f0a9e113535ba0ad3af354d8c90e7fff9fbe15
Shakambari123 <117158889+Shakambari123@users.noreply.github.com>
2025-10-23 13:30:43 +0530

    Assignment Devops first step
    Uncomment Flask and PostgreSQL connection code.

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
+# from flask import Flask
+# import psycopg2
+# import os
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     conn = psycopg2.connect(
+#         host=os.getenv("POSTGRES_HOST"),
+#         database=os.getenv("POSTGRES_DB"),
+#         user=os.getenv("POSTGRES_USER"),
+#         password=os.getenv("POSTGRES_PASSWORD")
+#     )
+#     cur = conn.cursor()
+#     cur.execute("SELECT version();")
+#     version = cur.fetchone()
+#     cur.close()
+#     conn.close()
+#     return f"Connected to PostgreSQL version: {version}"
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+
+
+
 from flask import Flask
 import psycopg2
 import os
-
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/')
 def home():
     conn = psycopg2.connect(
@@ -18,6 +44,6 @@ def home():
     cur.close()
     conn.close()
     return f"Connected to PostgreSQL version: {version}"
-
+ 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
